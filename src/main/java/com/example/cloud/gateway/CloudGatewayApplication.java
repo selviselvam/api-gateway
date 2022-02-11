d5b84a48d0523f5b6daea95a298d503560522956
selviselvam <selvicse04@gmail.com>
2022-02-11 15:17:32 +0530

    adding files

---
+++ src/main/java/com/example/cloud/gateway/CloudGatewayApplication.java
@@ -0,0 +1,16 @@
+package com.example.cloud.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+
+public class CloudGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudGatewayApplication.class, args);
+	}
+
+}
+//https://www.youtube.com/watch?v=b6R4dElDtRc
